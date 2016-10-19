7cbad966aaece7f1dd90f9be91fd6a616816921e
Taiki Arakawa <at.090313@gmail.com>
2016-10-19 10:25:33 +0900

    first commit

---
+++ src/resource.js
@@ -0,0 +1,11 @@
+var res = {
+   
+   leftbutton_png: "res/leftbutton.png",
+   rightbutton_png: "res/rightbutton.png",
+
+};
+
+var g_resources = [];
+for (var i in res) {
+   g_resources.push(res[i]);
+}
